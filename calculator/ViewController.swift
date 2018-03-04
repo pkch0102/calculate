2f4c8f1b7a467334a17e754626f131d411c1ebe0
박기찬 <pkch0102@naver.com>
2018-03-04 11:46:55 +0900

    오류 수정

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -13,18 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     var first: CLongLong!
     var caltype: Int!
+    var havetorefresh: Bool!
+    var error: Bool!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         first = 0
         caltype = -1
+        havetorefresh = false
+        error = false
     }
     
     @IBAction func allclear(_ sender: Any) {
         display.text = "0"
         first = 0
         caltype = -1
+        error = false
     }
+    
     @IBAction func calculate(_ sender: UIButton) {
         caltype = sender.tag
         first = CLongLong(display.text!)
@@ -32,6 +38,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func plusminus(_ sender: Any) {
+        if(error){
+            display.text = "오류"
+            return
+        }
         var tmp: CLongLong = CLongLong(display.text!)!
         tmp *= -1
         display.text = String(tmp)
@@ -40,17 +50,28 @@ class ViewController: UIViewController {
     @IBAction func equal(_ sender: Any) {
         let second: CLongLong = CLongLong(display.text!)!
         
+        if(error){
+            display.text = "오류"
+            return
+        }
+        
         if(caltype == 0){
             display.text = String(first + second)
         }else if(caltype == 1){
             display.text = String(first - second)
         }else if(caltype == 2){
+            if(second == 0){
+                error = true
+                display.text = "오류"
+                return
+            }
             display.text = String(first / second)
         }else if(caltype == 3){
             display.text = String(first * second)
         }else if(caltype == 4){
-            display.text = String(first & second)
+            display.text = String(first % second)
         }
+        havetorefresh = true
         first = 0
         caltype = -1
     }
@@ -79,8 +100,15 @@ class ViewController: UIViewController {
             return
         }
         
-       if(cnt == 1 && display.text! == "0") {
-             display.text = String(sender.tag)
+        if(error && display.text == "오류"){
+            display.text = String(sender.tag)
+            error = false
+            return
+        }
+        
+       if(cnt == 1 && display.text! == "0" || havetorefresh) {
+            havetorefresh = false
+            display.text = String(sender.tag)
         }else{
             display.text = display.text! + String(sender.tag)
         }
